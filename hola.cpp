f9605e3006752f8726f31f97b01aab594dc6ad2b
DT <epcbox123@gmail.com>
2018-04-06 23:04:47 +0200

    asdasd

---
+++ hola.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  const float pi = 3.1415;
+  float radio;
+  cin>>radio;
+  float area = pi*(radio*radio);
+  cout<<"el area es "<<area;
+  return 0;
+
+}
